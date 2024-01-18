90459290438eba401ef54c0f311242c0325ea353
emanuelmarcolongo <emanuelmarcolongo@gmail.com>
2024-01-18 16:41:50 -0300

    solve exercise 1

--- semana1/exercicio-1/src/App.java
+++ semana1/exercicio-1/src/App.java
@@ -1,5 +1,23 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        System.out.println(verifyNumber(2));
+        System.out.println(verifyNumber(1));
+        System.out.println(verifyNumber(-0));
+        System.out.println(verifyNumber(-1));
+        System.out.println(verifyNumber(-2));
+    }
+
+    static String verifyNumber(int num) {
+
+        if (num == 0) {
+            return "O numero 0 é neutro";
+        }
+
+        boolean isEven = (num % 2) == 0;
+        boolean isPositive = num > 0;
+
+        String message = "O numero " + num + " é " + (isEven ? " par " : " impar ") + " e "
+                + (isPositive ? " positivo " : " negativo ");
+        return message;
     }
 }
